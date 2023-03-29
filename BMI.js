b0e1dc87e6e9cfca1a2efd99da58710840e58bb5
NoorMohammadTalukder <noor11809027@gmail.com>
2023-03-30 02:19:09 +0600

    error fixed

--- BMI.js
+++ BMI.js
@@ -1,21 +1,22 @@
+function calculateBMI(weight, height) {
+  const output = document.querySelector("#output");
+  const error = document.querySelector("#error");
+  let BMI = weight / (height * height);
+
+  if (isNaN(BMI) || height == 0 || weight == 0) {
+    error.innerHTML = "Kindly Enter A Valid Input ";
+    output.innerHTML = "";
+  } else {
+    output.innerHTML = "Your BMI: " + BMI;
+    error.innerHTML = "";
+  }
+}
 const form = document.querySelector("#myForm");
-    form.addEventListener("submit", (event) => {
-      event.preventDefault(); 
-      const weightInput = document.querySelector("#weightInput");
-      const heightInput = document.querySelector("#heightInput");
-      const output = document.querySelector("#output");
-      const error = document.querySelector("#error");
-      const weight = parseFloat(weightInput.value);
-      const height = parseFloat(heightInput.value);
-      let BMI = weight / (height * height);
-     
-      if(isNaN(BMI) || height==0 || weight==0){
-        error.innerHTML = "Kindly Enter A Valid Input ";
-        output.innerHTML = "";
-      }else{
-        output.innerHTML = "Your BMI: " + BMI;
-        error.innerHTML = "";
-      }
-     
-      
-    });
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const weightInput = document.querySelector("#weightInput");
+  const heightInput = document.querySelector("#heightInput");
+  const weight = parseFloat(weightInput.value);
+  const height = parseFloat(heightInput.value);
+  calculateBMI(weight, height);
+});
